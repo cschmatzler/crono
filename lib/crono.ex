a00cb09da13db95e42e287995200c145eb304a0f
Christoph Schmatzler <christoph@medium.place>
2023-09-14 20:52:09 +0200

    rm boilerplate

--- lib/crono.ex
+++ lib/crono.ex
@@ -2,17 +2,4 @@ defmodule Crono do
   @moduledoc """
   Documentation for `Crono`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Crono.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
