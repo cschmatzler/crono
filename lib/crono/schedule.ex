bf02973c48ed8a6c06bb25a8f4586b60b74b99b5
Christoph Schmatzler <christoph@medium.place>
2023-09-15 17:54:45 +0200

    weekday schedule

--- lib/crono/schedule.ex
+++ lib/crono/schedule.ex
@@ -1,25 +1,44 @@
 defmodule Crono.Schedule do
   @moduledoc false
   def get_next_date(%Crono.Expression{} = expression, %DateTime{} = datetime \\ DateTime.utc_now()) do
-    expression
-    |> Crono.Expression.to_list()
-    |> get_date(datetime)
+    get_date(expression, Crono.Expression.to_list(expression), datetime)
   end
 
-  defp get_date([{_part, [:*]} | tail], datetime) do
-    get_date(tail, datetime)
+  defp get_date(expression, [{_part, [:*]} | tail], datetime) do
+    get_date(expression, tail, datetime)
   end
 
-  defp get_date([{part, value} | tail], datetime) do
-    get_date(tail, adjust_datetime(part, value, datetime))
+  defp get_date(expression, [{part, value} | tail], datetime) do
+    get_date(expression, tail, adjust_datetime(expression, part, value, datetime))
   end
 
-  defp get_date([], datetime), do: datetime |> Map.put(:second, 0) |> DateTime.truncate(:second)
+  defp get_date(_expression, [], datetime),
+    do: datetime |> Map.put(:second, 0) |> DateTime.truncate(:second)
 
-  @min [minute: 0, hour: 0, day: 1, month: 1]
-  @max [minute: 59, hour: 23, day: 31, month: 12]
+  @min [minute: 0, hour: 0, day: 1, month: 1, weekday: 0]
+  @max [minute: 59, hour: 23, day: 31, month: 12, weekday: 7]
   @next [minute: :hour, hour: :day, day: :month, month: :year]
-  defp adjust_datetime(type, [value], datetime)
+
+  defp adjust_datetime(expression, :weekday, [value], datetime) when is_integer(value) do
+    case datetime |> DateTime.to_date() |> Date.day_of_week() do
+      ^value -> datetime
+      _ -> get_next_date(expression, datetime)
+    end
+  end
+
+  defp adjust_datetime(expression, :weekday, [step: {:*, step}], datetime),
+    do: adjust_datetime_weekday_list(@min[:weekday]..@max[:weekday]//step, expression, datetime)
+
+  defp adjust_datetime(expression, :weekday, [step: {start, step}], datetime),
+    do: adjust_datetime_weekday_list(start..@max[:weekday]//step, expression, datetime)
+
+  defp adjust_datetime(expression, :weekday, [range: {from, to}], datetime),
+    do: adjust_datetime_weekday_list(from..to, expression, datetime)
+
+  defp adjust_datetime(expression, :weekday, [list: list], datetime),
+    do: adjust_datetime_weekday_list(list, expression, datetime)
+
+  defp adjust_datetime(_expression, type, [value], datetime)
        when is_integer(value) and type in [:minute, :hour, :day, :month] do
     if value > Map.get(datetime, type) do
       Map.put(datetime, type, value)
@@ -30,26 +49,27 @@ defmodule Crono.Schedule do
     end
   end
 
-  defp adjust_datetime(type, [step: {:*, step}], datetime)
+  defp adjust_datetime(expression, type, [step: {:*, step}], datetime)
        when type in [:minute, :hour, :day, :month],
-       do: adjust_datetime(type, [step: {@min[type], step}], datetime)
+       do: adjust_datetime(expression, type, [step: {@min[type], step}], datetime)
 
-  defp adjust_datetime(type, [step: {start, step}], datetime)
+  defp adjust_datetime(_expression, type, [step: {start, step}], datetime)
        when type in [:minute, :hour, :day, :month] do
     start..@max[type]//step
     |> Enum.to_list()
     |> adjust_datetime_list(type, datetime)
   end
 
-  defp adjust_datetime(type, [range: {from, to}], datetime)
+  defp adjust_datetime(_expression, type, [range: {from, to}], datetime)
        when type in [:minute, :hour, :day, :month] do
     from..to
     |> Enum.to_list()
     |> adjust_datetime_list(type, datetime)
   end
 
-  defp adjust_datetime(type, [list: list], datetime) when type in [:minute, :hour, :day, :month],
-    do: adjust_datetime_list(list, type, datetime)
+  defp adjust_datetime(_expression, type, [list: list], datetime)
+       when type in [:minute, :hour, :day, :month],
+       do: adjust_datetime_list(list, type, datetime)
 
   defp adjust_datetime_list(list, type, datetime) do
     datetime_value = Map.get(datetime, :type)
@@ -64,6 +84,14 @@ defmodule Crono.Schedule do
       |> Enum.drop_while(fn x -> x <= datetime_value end)
       |> List.first()
       |> then(&Map.put(datetime, type, &1))
+    end
+  end
+
+  defp adjust_datetime_weekday_list(list, expression, datetime) do
+    if datetime |> DateTime.to_date() |> Date.day_of_week() |> then(&Enum.member?(list, &1)) do
+      datetime
+    else
+      get_next_date(expression, datetime)
     end
   end
 end
